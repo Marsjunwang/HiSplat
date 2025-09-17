642c543e23a465a41e0e2a1aadcb0c22449b6de9
jun.wang03 <jun.wang03@narwal.com>
2025-09-17 11:43:36 +0800

    local test

--- rsync.sh
+++ rsync.sh
@@ -1,10 +1,9 @@
-# A800
-rsync -avz --progress --exclude='outputs' \
-    --exclude='*.pyc' --exclude='__pycache__' \
-    --exclude='.git' --exclude='latest-run'\
-    -e "ssh -p 30343" ./ root@10.8.150.32:/mnt/data/jun.wang03/HiSplat/
-
-# # L40
+# # A800
 # rsync -avz --progress --exclude='outputs' \
-#     --exclude='*.pyc' --exclude='__pycache__' --exclude='build' \
-#     -e "ssh" ./ narwal@192.168.31.214:/home/narwal/HiSplat/
+#     --exclude='*.pyc' --exclude='__pycache__' \
+#     --exclude='.git' --exclude='latest-run'\
+#     -e "ssh -p 30343" ./ root@10.8.150.32:/mnt/data/jun.wang03/HiSplat/
+
+# L40 # --exclude='*.pyc' --exclude='__pycache__' --exclude='build' \
+rsync -avz --progress --exclude='outputs' \
+    -e "ssh" ./ narwal@192.168.31.214:/home/narwal/HiSplat/
